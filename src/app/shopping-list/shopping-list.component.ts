c9058e149396a780a46f98bc0a4bd56234a19491
Jean-Phi <azerato.graphe@gmail.com>
2020-03-30 14:55:17 -0400

    shopping list style

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Tomato", 5),
+    new Ingredient("Potatoes", 5),
+  ];
 
   constructor() { }
 
